a3251a47e5a5f66e1ececeb45d9222312f5f64bc
Holly Williams <williamsh@objectcomputing.com>
2019-04-11 16:31:11 -0500

    more rest

---
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springdemo.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springdemo.entity.Student;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+	private List<Student> theStudents;
+	
+	@PostConstruct
+	public void LoadData() {
+		
+		theStudents = new ArrayList<>();
+		
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+		
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
+		return theStudents;
+	}
+	
+}
--- spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -7,9 +7,14 @@
 Spring Rest demo
 
 <hr>
+<br><br>
 
 <!-- this also works    <a href="test/hello">Hello</a>   -->
 <a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+
+<hr>
+<br><br>
+<a href="${pageContext.request.contextPath}/api/students">Get Student List</a>
 
 </body>
 
